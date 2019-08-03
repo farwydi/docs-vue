593b427ab518e62b23e969e6e14a3592d6f8b1e3
Leonid Zharikov <leon.zharikov@gmail.com>
2019-08-03 18:59:49 +0300

    feat; explore

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,5 @@
 <template>
     <main>
-        <section class="section">
-            <div class="container">
-                <nav
-                    class="navbar"
-                    role="navigation"
-                    aria-label="main navigation"
-                >
-                    <div class="navbar-brand">
-                        <a class="navbar-item" href="https://bulma.io">
-                            <img
-                                src="https://bulma.io/images/bulma-logo.png"
-                                width="112"
-                                height="28"
-                            />
-                        </a>
-
-                        <a
-                            role="button"
-                            class="navbar-burger burger"
-                            aria-label="menu"
-                            aria-expanded="false"
-                            data-target="navbarBasicExample"
-                        >
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                            <span aria-hidden="true"></span>
-                        </a>
-                    </div>
-
-                    <div id="navbarBasicExample" class="navbar-menu">
-                        <div class="navbar-start">
-                            <a class="navbar-item">
-                                Home
-                            </a>
-
-                            <a class="navbar-item">
-                                Documentation
-                            </a>
-
-                            <div class="navbar-item has-dropdown is-hoverable">
-                                <a class="navbar-link">
-                                    More
-                                </a>
-
-                                <div class="navbar-dropdown">
-                                    <a class="navbar-item">
-                                        About
-                                    </a>
-                                    <a class="navbar-item">
-                                        Jobs
-                                    </a>
-                                    <a class="navbar-item">
-                                        Contact
-                                    </a>
-                                    <hr class="navbar-divider" />
-                                    <a class="navbar-item">
-                                        Report an issue
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="navbar-end">
-                            <div class="navbar-item">
-                                <div class="buttons">
-                                    <a class="button is-primary">
-                                        <strong>Sign up</strong>
-                                    </a>
-                                    <a class="button is-light">
-                                        Log in
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </section>
-
         <section class="hero">
             <div class="hero-body">
                 <div class="container">
@@ -98,12 +19,13 @@
                 </div>
             </div>
         </section>
+
         <section class="section">
-            <Markdown
-                class="container"
-                page_url="https://raw.githubusercontent.com/axios/axios/master/README.md"
-            ></Markdown>
+            <div class="container">
+                <router-view></router-view>
+            </div>
         </section>
+
         <footer class="footer">
             <div class="content has-text-centered">
                 <p>
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ProjectExplorer from './views/ProjectExplorer.vue'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: () =>
-                import(/* webpackChunkName: "about" */ './views/About.vue')
+            path: '/project-explore',
+            name: 'project-explore',
+            component: ProjectExplorer
         }
     ]
 })
